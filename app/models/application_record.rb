5fe8e33cfb6b2ed7e1579cdac8f29b568a0007de
CeMuPaMuDa <bepyc9i@gmail.com>
2021-12-16 17:36:11 +0200

    readme fix

--- app/models/application_record.rb
+++ app/models/application_record.rb
@@ -4,6 +4,6 @@ class ApplicationRecord < ActiveRecord::Base
   self.abstract_class = true
 
   def deadline_valid?
-    errors.add(:deadline, 'Cannot be less than the current date') if deadline < Time.now
+    errors.add(:deadline, 'Cannot be less than the current date') if deadline <= Time.now
   end
 end
