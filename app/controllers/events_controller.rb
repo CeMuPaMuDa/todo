95c9cb9da1296b8e7884ef35fd33b12f372be4d1
CeMuPaMuDa <bepyc9i@gmail.com>
2022-01-17 12:57:49 +0200

    edit form

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -53,6 +53,6 @@ class EventsController < ApplicationController
 
     # Only allow a list of trusted parameters through.
     def event_params
-      params.fetch(:event, {})
+      params.require(:event).permit(:title, :description, :completed)
     end
 end
