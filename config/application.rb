f91f518f290cbff0bb8c2c7011e89e1fb1211f99
CeMuPaMuDa <bepyc9i@gmail.com>
2022-01-31 14:41:11 +0200

    add standart l18n yml files

--- config/application.rb
+++ config/application.rb
@@ -14,10 +14,11 @@ module Todo
   class Application < Rails::Application
 
     config.load_defaults 6.1
-    config.i18n.available_locales = %i[en ru]
+    config.i18n.available_locales = %i[en ru uk]
     config.i18n.default_locale = :ru
     config.time_zone = 'Kyiv'
-    config.eager_load_paths += Dir["#{config.root}/lib/**/"]
+    config.eager_load_paths += Dir["#{config.root}/lib/**/",
+                                  "#{config.root}/config/locales/**/"]
     config.generators do |g|
       g.orm              :active_record
       g.template_engine  :slim
