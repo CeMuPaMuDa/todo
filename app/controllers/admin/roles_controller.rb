ce850b2d3ecb753b6f0ec2b5ae7fa1761121aba7
CeMuPaMuDa <bepyc9i@gmail.com>
2022-02-08 02:37:29 +0200

    add breadcrumb(user,role)

--- app/controllers/admin/roles_controller.rb
+++ app/controllers/admin/roles_controller.rb
@@ -3,6 +3,8 @@
 module Admin
   class RolesController < Admin::ApplicationController
     before_action :set_admin_role, only: %i[show edit update destroy]
+    add_breadcrumb 'Admin Panel', :admin_root_path
+    before_action :add_roles_admin_breadcrumb, only: %i[index show edit new]
 
     # GET /admin/roles
     def index
@@ -13,17 +15,21 @@ module Admin
     # GET /admin/roles/1
     def show
       authorize [:admin, @admin_role]
+      add_breadcrumb "#{@admin_role.alias_name}}", admin_role_path
     end
 
     # GET /admin/roles/new
     def new
       @admin_role = Role.new
       authorize [:admin, @admin_role]
+      add_breadcrumb 'New Role', new_admin_role_path
     end
 
     # GET /admin/roles/1/edit
     def edit
       authorize [:admin, @admin_role]
+      add_breadcrumb "Role: #{@admin_role.alias_name}", admin_role_path(@admin_role)
+      add_breadcrumb 'Edit Role', edit_admin_role_path
     end
 
     # POST /admin/roles
@@ -65,6 +71,10 @@ module Admin
     def admin_role_params
       # params.fetch(:admin_role, {})
       params.require(:role).permit(:name, :alias_name)
+    end
+
+    def add_roles_admin_breadcrumb
+      add_breadcrumb 'Roles', admin_roles_path
     end
   end
 end
