3178f297687caa0490bdc968bbf0341e8f4b6b19
CeMuPaMuDa <bepyc9i@gmail.com>
2022-02-16 17:37:34 +0200

    comment N+1

--- app/api/helpers/events_helper.rb
+++ app/api/helpers/events_helper.rb
@@ -4,7 +4,8 @@ module EventsHelper
   extend Grape::API::Helpers
 
   def events_scope(all)
-    scope = Event.order(:id).includes(user: :role)
+    scope = Event.order(:id)
+                #  .includes(user: :role)
     all ? scope : scope.where(completed: false)
   end
 end
