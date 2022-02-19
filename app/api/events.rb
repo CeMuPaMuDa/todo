3ff5520e6303864e68120bd37bd2fe1a88e922d5
CeMuPaMuDa <bepyc9i@gmail.com>
2022-02-19 02:14:22 +0200

    grape kaminari

--- app/api/events.rb
+++ app/api/events.rb
@@ -1,16 +1,18 @@
 # frozen_string_literal: true
 
 class Events < Grape::API
+  include Grape::Kaminari
   helpers FiltersHelper, EventsHelper
 
   resource :events do
     desc 'Список дел'
     params do
       use :filters
+      use :pagination, per_page: 5, max_per_page: 5, offset: 0
     end
 
     get '/' do
-      present events_scope(params[:all]), with: Entities::EventIndex
+      present paginate(events_scope(params[:all])), with: Entities::EventIndex
     end
 
     route_param :event_id, type: Integer do
