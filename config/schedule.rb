f1dec182a2d08e0ec1a49270b7da263c50beee36
CeMuPaMuDa <bepyc9i@gmail.com>
2022-03-11 19:36:35 +0200

    fix

--- config/schedule.rb
+++ config/schedule.rb
@@ -2,7 +2,7 @@ env :PATH, ENV['PATH']
 set :environment, ENV['RAILS_ENV']
 set :output, error: 'log/cron_error.log', standard: 'log/cron.log'
 
-every 1.minute do
+every 1.day do
   rake "mails:event_deadline"
 end
 
